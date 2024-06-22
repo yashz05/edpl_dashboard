4d427fdc64082160ffc679150694e855d31cddcf
Yash Chowdhari <yashc1999@gmail.com>
2024-06-22 06:42:50 +0530

    new server

--- src/providers/data-provider/index.ts
+++ src/providers/data-provider/index.ts
@@ -4,8 +4,8 @@
 import dataProviderSimpleRest from "./../../datapr/";
 import axios from "axios";
 
-const API_URL = "http://207.180.252.68:8092/api";
-export const photos = "http://207.180.252.68:8092/assets/uploads/";
+const API_URL = "http://156.67.80.227:8092/api";
+export const photos = "http://156.67.80.227:8092/assets/uploads/";
 // const API_URL = "https://api.fake-rest.refine.dev";
 
 const dataProvider = dataProviderSimpleRest(API_URL);
