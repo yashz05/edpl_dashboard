8d230ad5d4ca432975173935b89be85ae015ff05
Yash Chowdhari <yashc1999@gmail.com>
2024-06-30 17:22:12 +0530

    urls

--- src/providers/data-provider/index.ts
+++ src/providers/data-provider/index.ts
@@ -3,12 +3,20 @@
 
 import dataProviderSimpleRest from "./../../datapr/";
 import axios from "axios";
-
-
+export var API_URL = ''
+export var photos = ''
 // UAT
 // http://207.180.252.68:8092/
-export const API_URL = "http://207.180.252.68:8092/api";
-export const photos = "http://207.180.252.68:8092/assets/uploads/";
+if (process.env.NODE_ENV === 'production') {
+    API_URL = "https://salesbe.eurodecor.co.in/api";
+    photos = "https://salesbe.eurodecor.co.in/assets/uploads/";
+} else {
+    API_URL = "http://207.180.252.68:8092/api";
+    photos = "http://207.180.252.68:8092/assets/uploads/";
+}
+
+
+
 // const API_URL = "https://api.fake-rest.refine.dev";
 
 const dataProvider = dataProviderSimpleRest(API_URL);
