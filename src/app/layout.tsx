e33b519a300180aae2b1d3fc98b5d1a7f2a66028
Yash Chowdhari <yashc1999@gmail.com>
2024-09-24 13:33:05 +0530

    added company back

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -107,16 +107,16 @@ export default function RootLayout({
     },
 
     {
-      name: "auth/sales_team",
-      list: "/salesPerson",
-      create: "/salesPerson/create",
-      edit: "/salesPerson/edit/:id",
-      show: "/salesPerson/show/:id",
+      name: "edpl/company",
+      list: "/companies",
+      create: "/companies/create",
+      edit: "/companies/edit/:id",
+      show: "/companies/show/:id",
       meta: {
-        parent: 'Settings',
-        label: 'Users',
+
+        label: 'Companies',
         canDelete: true,
-        icon: <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-users"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M9 7m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0" /><path d="M3 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" /><path d="M16 3.13a4 4 0 0 1 0 7.75" /><path d="M21 21v-2a4 4 0 0 0 -3 -3.85" /></svg>
+        icon: <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-building-factory-2"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M3 21h18" /><path d="M5 21v-12l5 4v-4l5 4h4" /><path d="M19 21v-8l-1.436 -9.574a.5 .5 0 0 0 -.495 -.426h-1.145a.5 .5 0 0 0 -.494 .418l-1.43 8.582" /><path d="M9 17h1" /><path d="M14 17h1" /></svg>
       },
     },
     {
